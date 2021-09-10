573597d6c80e241d8116101a165d588b6f1784de
R1sKD <denis07011997q@gmail.com>
2021-09-10 20:24:12 +0300

    feat: half-solve recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+   NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement class DepthCalculator with method calculateDepth
@@ -13,8 +15,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   calculateDepth(arr) {
+      let res = 0;
+      let newRes = 1;
+      for (let i = 0; i < arr.length; i++) {
+         if (Array.isArray(arr[i])) {
+            newRes += this.calculateDepth(arr[i]);
+         }
+         if (newRes > res) {
+            res = newRes;
+         }
+      }
+      return res;
+   }
 }
