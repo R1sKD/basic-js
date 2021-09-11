ea9a8ddab94df4c319dbf482d932cadd90098ef0
R1sKD <denis07011997q@gmail.com>
2021-09-11 17:13:48 +0300

    feat: half solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,36 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+   NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   link: [],
+   getLength() {
+      return this.link.length;
+   },
+   addLink(value) {
+      if (value === undefined) {
+         value = '';
+      }
+      this.link.push(`( ${value} )`);
+      return this;
+   },
+   removeLink(position) {
+      if (position < 0 || position > this.link.length || typeof position != 'number') {
+         throw new Error('You can\'t remove incorrect link!');
+      };
+      this.link.splice(--position, 1);
+      return this;
+   },
+   reverseChain() {
+      this.link.reverse();
+      return this;
+   },
+   finishChain() {
+      let res = this.link.join('~~');
+      return res;
+   }
 };
