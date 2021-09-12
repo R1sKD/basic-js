d7df30798f867a669066fd86a80cf9a1a5b6835b
R1sKD <denis07011997q@gmail.com>
2021-09-12 14:02:39 +0300

    feat: solve st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,61 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  let res = [];
+  let bombs = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    res.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let mainIndex = i;
+      let subIndex = j;
+      let mainTopIndex = mainIndex - 1 < 0 ? false : (mainIndex - 1).toString();
+      let mainBottomIndex = mainIndex + 1 > matrix.length - 1 ? false : mainIndex + 1;
+      let subLeftIndex = subIndex - 1 < 0 ? false : (subIndex - 1).toString();
+      let subRightIndex = subIndex + 1 > matrix[i][j].length - 1 ? false : subIndex + 1;
+      if (subLeftIndex && mainTopIndex) {
+        if (matrix[mainTopIndex][subLeftIndex] === true) {
+          bombs++;
+        }
+      }
+      if (mainTopIndex) {
+        if (matrix[mainTopIndex][subIndex] === true) {
+          bombs++;
+        }
+      }
+      if (mainTopIndex && subRightIndex) {
+        if (matrix[mainTopIndex][subRightIndex] === true) {
+          bombs++;
+        }
+      }
+      if (subLeftIndex) {
+        if (matrix[mainIndex][subLeftIndex] === true) {
+          bombs++;
+        }
+      }
+      if (subRightIndex) {
+        if (matrix[mainIndex][subRightIndex] === true) {
+          bombs++;
+        }
+      }
+      if (subLeftIndex && mainBottomIndex) {
+        if (matrix[mainBottomIndex][subLeftIndex] === true) {
+          bombs++;
+        }
+      }
+      if (mainBottomIndex) {
+        if (matrix[mainBottomIndex][subIndex] === true) {
+          bombs++;
+        }
+      }
+      if (mainBottomIndex && subRightIndex) {
+        if (matrix[mainBottomIndex][subRightIndex] === true) {
+          bombs++;
+        }
+      }
+      res[i].push(bombs);
+      bombs = 0;
+    }
+  }
+  return res;
 }
