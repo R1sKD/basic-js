891cfab0964c154650f4587345a7d8011a787c7f
R1sKD <denis07011997q@gmail.com>
2021-09-12 14:34:01 +0300

    feat: solve st-sum-digits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  let res = n.toString().split('').reduce((s, num) => s + +num, 0).toString();
+  return res.length === 1 ? +res : getSumOfDigits(+res);
 }
