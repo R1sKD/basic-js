99bdd2f89e5cd6dde0e38987553d2428c38ff32c
Denis Prokhorenko <denis07011997q@gmail.com>
2021-09-18 17:37:12 +0300

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity.trim() === '') return false;
+  if (typeof (+sampleActivity) !== 'number' || isNaN(+sampleActivity) || +sampleActivity <= 0 || +sampleActivity === 9000) return false;
+  if (+sampleActivity > 15) {
+    if (!((+sampleActivity ^ 0) === +sampleActivity)) {
+      return false;
+    }
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 }
+
