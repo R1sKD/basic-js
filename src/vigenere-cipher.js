406ffb6be08e49dd39877e0b3238b19694fa9f4e
Denis Prokhorenko <denis07011997q@gmail.com>
2021-10-03 13:01:29 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,17 +20,74 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  constructor (mod) {
+  constructor (mod = true) {
     this.mod = mod;
   }
   encrypt(string, key) {
     if (string === undefined || key === undefined) {
       throw new Error('Incorrect arguments!');
     }
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    let kf = Math.ceil(string.length / key.length);
+    key = key.repeat(kf);
+
+    let codeA = "A".charCodeAt(0);
+    let abcCount = 26;
+
+    let result = [];
+
+    let j = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === ' ' || string[i].charCodeAt(0) < 65 || string[i].charCodeAt(0) > 90) {
+        result.push(string[i]);
+      } else {
+        let letterIdx = string.charCodeAt(i) - codeA;
+        let shift = key.charCodeAt(j) - codeA;
+        j++;
+
+        result.push(
+          String.fromCharCode(codeA + (letterIdx + shift) % abcCount)
+        );
+      }
+    }
+
+    return this.mod === false ? result.reverse().join('') : result.join('');
   }
   decrypt(string, key) {
     if (string === undefined || key === undefined) {
       throw new Error('Incorrect arguments!');
     }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+    
+    //string = this.encrypted === string ? string : string.split('').reverse().join('') === string ? string.split('').reverse().join('') : string;
+
+    let kf = Math.ceil(string.length / key.length);
+    key = key.repeat(kf);
+
+    let codeA = "A".charCodeAt(0);
+    let abcCount = 26;
+
+    let result = [];
+
+    let j = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === ' ' || string[i].charCodeAt(0) < 65 || string[i].charCodeAt(0) > 90) {
+        result.push(string[i]);
+      } else {
+        let letterIdx = string.charCodeAt(i) - codeA;
+        let shift = key.charCodeAt(j) - codeA;
+        j++;
+
+        result.push(
+          String.fromCharCode(codeA + (letterIdx - shift + abcCount) % abcCount)
+        );
+      }
+    }
+
+    return this.mod === false ? result.reverse().join('') : result.join('');
   }
 }
